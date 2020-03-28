91d0c60d75ac8a697a441dae65a8a6ae845d0874
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-03-28 12:56:39 -0300

    feat(backend): usa JSON como padrão

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const express = require('express');
 // Define a aplicação
 const app = express();
 
+// Define o JSON para as requisições
+app.use(express.json());
+
 // Main route
 app.get('/', (req, res) => {
   return res.json({ message: 'Hello World!'});
