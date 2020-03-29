73736700c15f5d0ee0e3ee9f9625e5ab2050660a
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-03-29 11:33:05 -0300

    feat(backend): adicionada paginação dos casos

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -3,7 +3,17 @@ const connection = require('../database/connection');
 
 module.exports = {
   async index(req, res) {
-    const incidents = await connection('incidents').select('*');
+    const { page = 1 } = req.query;
+
+    const [count] = await connection('incidents').count();
+
+    const incidents = await connection('incidents')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf']);
+
+    res.header('X-Total-Count', count['count(*)']);
 
     return res.json(incidents);
   },
@@ -12,7 +22,7 @@ module.exports = {
     const { title, description, value } = req.body;
     const ong_id = req.headers.authorization;
 
-    const [ id ] = await connection('incidents').insert({
+    const [id] = await connection('incidents').insert({
       title,
       description,
       value,
@@ -25,14 +35,14 @@ module.exports = {
   async delete(req, res) {
     const { id } = req.params;
     const ong_id = req.headers.authorization;
-    
+
     const incident = await connection('incidents')
       .where('id', id)
       .select('ong_id')
       .first();
 
     if (incident.ong_id !== ong_id) {
-      return res.status(401).json({ error: 'Operation not permitted'});
+      return res.status(401).json({ error: 'Operation not permitted' });
     }
 
     await connection('incidents').where('id', id).delete();
