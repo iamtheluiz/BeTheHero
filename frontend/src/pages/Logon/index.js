38d9faa243a1026701a8d2da3205643cfe65c4ea
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-03-29 19:52:49 -0300

    feat(frontend): integração do logon com o backend

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 // Styles
 import './styles.css';
@@ -9,15 +11,37 @@ import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
+  const [id, setId] = useState("");
+  const history = useHistory();
+
+  async function handleLogin(event) {
+    event.preventDefault();
+
+    try {
+      const response = await api.post('sessions', { id });
+
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+
+      history.push('/profile');
+    } catch (error) {
+      alert('Falha no login, tente novamente!');
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
-        <img src={logoImg} alt="Be The Hero"/>
-        
-        <form>
+        <img src={logoImg} alt="Be The Hero" />
+
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
 
-          <input placeholder="Sua ID" />
+          <input
+            placeholder="Sua ID"
+            value={id}
+            onChange={event => setId(event.target.value)}
+          />
           <button type="submit" className="button">Entrar</button>
 
           <Link className="back-link" to="/register">
@@ -27,7 +51,7 @@ export default function Logon() {
         </form>
       </section>
 
-      <img src={heroesImg} alt="Heroes"/>
+      <img src={heroesImg} alt="Heroes" />
     </div>
   );
 }
