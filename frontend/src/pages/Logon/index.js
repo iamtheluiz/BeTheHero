76230b46ed67da9cdb02b411aac5e3969a253937
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-03-29 19:14:15 -0300

    feat(frontend): criada página de cadastro

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 // Styles
@@ -19,10 +20,10 @@ export default function Logon() {
           <input placeholder="Sua ID" />
           <button type="submit" className="button">Entrar</button>
 
-          <a href="/register">
+          <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#E02041" />
             Não tenho cadastro
-          </a>
+          </Link>
         </form>
       </section>
 
